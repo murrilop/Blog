258d7387cca59d2f089af28a61c29c2e4fe407e2
murrilop <murilogonzagasilva14@gmail.com>
2024-10-16 01:01:49 -0300

    tela de post finalizada

--- post.php
+++ post.php
@@ -23,28 +23,27 @@ if(isset($_GET['id'])){
         Cum repudiandae praesentium aperiam nobis aliquam nemo perferendis ad incidunt veritatis nesciunt ducimus voluptates inventore ea, assumenda quos, nulla quasi fuga suscipit atque. Itaque blanditiis id soluta dolore optio ad!
         Quis enim adipisci nulla, repellendus quasi illum, in assumenda consequuntur deleniti repudiandae fugiat qui quam. Ipsum, vel sequi mollitia et aliquam maiores ratione nulla rerum quaerat magni eius, exercitationem sunt!
         Nisi officiis quibusdam dolores, sapiente temporibus tempora odit ipsam voluptate fuga repellendus modi doloribus voluptatem animi nam. Distinctio hic veniam illum tempora, exercitationem eligendi, totam quis iusto, a quas explicabo!</p>
-    </div>
         <p class="post-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam laborum blanditiis aliquid molestias deleniti facilis sapiente enim doloremque asperiores temporibus placeat, nobis molestiae vitae ipsa! Libero itaque error illum eius.
         Accusantium, consequuntur similique, cupiditate dolore exercitationem iusto quo voluptas laboriosam labore, nihil illum? Delectus, et, corrupti qui nisi quam cupiditate eaque quaerat provident sequi repellendus non dolorem labore dicta laudantium.
         Cum repudiandae praesentium aperiam nobis aliquam nemo perferendis ad incidunt veritatis nesciunt ducimus voluptates inventore ea, assumenda quos, nulla quasi fuga suscipit atque. Itaque blanditiis id soluta dolore optio ad!
         Quis enim adipisci nulla, repellendus quasi illum, in assumenda consequuntur deleniti repudiandae fugiat qui quam. Ipsum, vel sequi mollitia et aliquam maiores ratione nulla rerum quaerat magni eius, exercitationem sunt!
         Nisi officiis quibusdam dolores, sapiente temporibus tempora odit ipsam voluptate fuga repellendus modi doloribus voluptatem animi nam. Distinctio hic veniam illum tempora, exercitationem eligendi, totam quis iusto, a quas explicabo!</p>
     </div>
+    <aside id="nav-container">
+        <h3 id="tags-title">Tags</h3>
+        <ul id="tag-list">
+            <?php foreach($currentPost['tags'] as $tag): ?>
+                <li><a href=""><?php echo $tag; ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+        <h3 id="categories-title">Categorias</h3>
+        <ul id="categories-list">
+            <?php foreach($categories as $category): ?>
+                <li><a href=""><?php echo $category; ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+    </aside>
 </main>
-<aside id="nav-container">
-    <h3 id="tags-title">Tags</h3>
-    <ul id="tag-list">
-        <?php foreach($currentPost['tags'] as $tag): ?>
-            <li><a href=""><?php echo $tag; ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-    <h3 id="categories-title">Categorias</h3>
-    <ul id="categories-list">
-        <?php foreach($categories as $category): ?>
-            <li><a href=""><?php echo $category; ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-</aside>
 <?php
 include_once('templates/footer.php');
 ?>
